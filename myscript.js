0c090edab87089869469b2578cb677c4ef58e79a
enrico76 <enricodip@gmail.com>
2019-07-22 09:24:43 +0200

    dichiarazioni variabili script

--- myscript.js
+++ myscript.js
@@ -1,1 +1,8 @@
 console.log("initial script");
+
+$(".arrowright").click(avanti);
+
+function avanti() {
+  var activeImm = $(".galleria img.active");
+  var activedot = $(".containerdots span.active");
+}
